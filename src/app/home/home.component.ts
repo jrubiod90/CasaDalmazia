08f85376b5e5f0717f5f82f39655cd3151e50842
jorgerubiodiez <jorgerubiodiez@outlook.com>
2025-07-02 23:31:09 +0200

    Solve images problems

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,7 +16,7 @@ export class HomeComponent {
   }
 
   openLinkInNewTab() {
-    const url = 'https://www.airbnb.es/rooms/945766271880521378?guests=1&adults=1&s=67&unique_share_id=f7ca4270-2099-4e9a-adab-1682e1fe5186';
+    const url = '';
     window.open(url, '_blank');
   }
 }
